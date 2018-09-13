e5899adb1b5355bef42b2a8d370aa158babbafbf
Miguel Angel <devmiguelangel@gmail.com>
2018-09-13 16:50:45 -0400

    queries

--- src/App.js
+++ src/App.js
@@ -1,18 +1,63 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const GET_COURSES = gql`
+  {
+    courses {
+      title
+      description
+      professor {
+        name
+      }
+    }
+  }
+`;
+
+const Courses = () => (
+  <Query query={GET_COURSES}>
+    {
+      ({ loading, error, data }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>{error} :(</p>
+        
+        return (
+          <Fragment>
+            {
+              data.courses.map((course, index) =>
+                <div className="row border rounded mb-2" style={{ backgroundColor: '#ECEFF1' }} key={index}>
+                  <div className="col p-2">
+                    <h4>{course.title}</h4>
+                    <p className="lead" style={{ fontSize: '.8rem', textAlign: 'justify', }}>
+                      {course.description}
+                    </p>
+                    <h5>Profesor: 
+                      <span className="badge badge-secondary">
+                        {course.professor.name}
+                      </span>
+                    </h5>
+                  </div>
+                </div>
+              )
+            }
+          </Fragment>
+        )
+      }
+    }
+  </Query>
+);
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="container">
+        <h1>Lista de Cursos</h1>
+
+        <Courses />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ApolloClient from 'apollo-boost';
+import gql from 'graphql-tag';
+import { ApolloProvider } from 'react-apollo';
+
+
+const client = new ApolloClient({
+  uri: '/api',
+})
+
+client.
+  query({
+    query: gql`
+      {
+        courses {
+          titles
+          description
+          professor {
+            name
+            nationality
+            gender
+          }
+        }
+      }
+    `
+  })
+  .then(result => {
+    console.log(result);
+  });
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>
+, document.getElementById('root'));
 registerServiceWorker();
